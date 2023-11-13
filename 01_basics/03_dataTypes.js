2684cc19f61b5da1821c3a931d217abd3b532d12
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-13 07:12:11 +0000

    03_dataypes

---
+++ 01_basics/03_dataTypes.js
@@ -0,0 +1,30 @@
+'use strict'; // will be treated as newer js version. by default bhi strict hi hota hai.
+
+// alert(3+3)  we are using nodejs, not browser
+
+// code readability matters so we won't do like below.
+
+console.log(3+3); console.log('Manish')
+
+let name = 'manish'
+let age = 23
+let isLoggedIn = true
+
+//number => 2 raise to 53
+//bigint
+//string => ''
+//boolean => true or false
+//undefined => youve not defined value,
+
+//null => standalone value hai, yes datatype bhi hai.
+// null ek representation hai empty value ka
+let state = null
+
+//symbol => unique 
+
+//object
+console.log(typeof 'manish');
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
+
