17c21338a358243382678e4a805a88f8b84f4e4e
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-13 06:37:02 +0000

    01_variables

---
+++ 01_basics/02_variables.js
@@ -0,0 +1,20 @@
+const accountId = 123
+let accountEmail = 'manish@google.com'
+var accountPwd = '12345'
+accountCity = 'Jaipur'
+let accountState;
+
+
+// accountId = 2         not allowed, TypeError: Assignment to constant variable.
+// console.log(accountId);
+
+accountEmail='hc@hc.com'
+accountPwd='121212'
+accountCity='Bengaluru'
+
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState])
+
+
+
+
+
