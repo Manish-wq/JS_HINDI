162fcf420139fc8b9fc5437be2cc52c5b58d2d73
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-13 17:07:15 +0000

    comparisons_datatypes

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,21 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2<=1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log('2'>11)
+// console.log('02'>11)
+
+// console.log(null > 0);
+// console.log(null==0);
+// console.log(null>=0)
+
+// console.log(undefined==0);
+// console.log(undefined<0);
+// console.log(undefined>0);
+
+// === (strict check)
+ console.log('2'=== 2);
+ console.log(2 === 2);
