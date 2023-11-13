b2bd5b845030c8294cc037dc9eaa08f9a5b9405a
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-13 16:07:06 +0000

    conversion_Operations

---
+++ 01_basics/04_conversionOperation.js
@@ -0,0 +1,29 @@
+// let score = '33abc'
+// let score = null
+// let score = undefined
+// let score = false
+let score = 'manish'
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log( typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// '33' => 33
+// '33abc' => NaN
+// true = 1, false = 0
+// '' => false
+// 'manish' = true
+ let isLoggedIn = 0
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanIsLoggedIn);
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
