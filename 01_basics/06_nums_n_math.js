1d86bf349df4bc2f86795108cee7b2a1fe4d97a6
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-14 11:30:26 +0000

    math_n_nums

---
+++ 01_basics/06_nums_n_math.js
@@ -0,0 +1,49 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+// const otherNumber = 23.8966
+const otherNumber = 123.8966
+// const otherNumber = 1123.8966
+// console.log(otherNumber.toPrecision(4))
+
+
+const hundreds = 10000000
+// console.log(hundreds.toLocaleString())  //by default international
+// console.log(hundreds.toLocaleString('en-IN'))
+
+
+// ______________Maths_____________________
+
+// console.log(Math)
+// console.log(Math.abs(-3))
+// console.log(Math.round(4.5))  //mostly used
+
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.7))
+
+// console.log(Math.sqrt(20))
+// console.log(Math.sqrt(400))
+
+// console.log(Math.SQRT2)
+// console.log(Math.SQRT1_2)
+
+// console.log(Math.min(1,2,4,5,0))
+// console.log(Math.max(1,2,4,5,0))
+
+// console.log(Math.random())
+console.log((Math.random()*10) + 1)
+console.log(Math.floor(Math.random()*10) + 1)
+
+const min = 10
+const max = 20
+
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
