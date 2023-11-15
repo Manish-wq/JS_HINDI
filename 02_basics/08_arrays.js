ff5476da85db3655261804b7d8a8051ee340daa2
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-15 06:22:21 +0000

    arrays01

---
+++ 02_basics/08_arrays.js
@@ -0,0 +1,40 @@
+// Arrays
+
+const myArr = [1, 5, 6, 7, 99, 77]
+const myHeros = ['shaktiman','naagraj']
+
+const myArr2 = new Array(1, 2, 3, 5)
+
+// console.log(myArr[0]);
+// console.log(myArr[1]);
+
+//  Array methods
+
+// myArr.push(33)
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(11)        //load ni ane dena hai.
+// myArr.shift()
+// console.log(myArr.includes(5))
+// console.log(myArr.indexOf(9))  //-1 if element not present.
+// console.log(myArr.indexOf(5))
+
+
+
+// const newArr = myArr.join()
+// console.log(newArr);
+// console.log(typeof newArr);
+
+//slice_splice
+
+
+const myn1 = myArr.slice(1,3)
+console.log(myn1);
+console.log('B', myArr )
+
+const myn2 = myArr.splice(1,3)
+console.log(myArr)
+console.log(myn2);
+
+
+
