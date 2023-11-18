65096cd56dbec3e42c834edbb474b161976efab5
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-18 04:57:17 +0000

    switch_case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+// switch  case 
+
+// const month = 'february'
+
+// switch (month) {
+//     case 'January':
+//         console.log('January')
+//         break;
+//     case 'February':
+//         console.log('February')
+//         break;
+//     case 'March':
+//         console.log('March')
+//         break;
+//     case 4:
+//         console.log('April')
+//         break;
+
+//     default:
+//         console.log('default case matched')
+//         break;
+// }
+
+
+
