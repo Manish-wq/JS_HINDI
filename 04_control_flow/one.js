abfefd92c764655086265283220bf51a5434d46f
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-18 04:56:12 +0000

    if-else

---
+++ 04_control_flow/one.js
@@ -0,0 +1,68 @@
+// if
+
+const isUserLoggedIn = true
+const temperature = 51
+
+// if (2!==2){
+//     console.log('executed')
+// }
+ 
+// if (temperature===50) {
+//     console.log('Temperature is less than 50')
+// } 
+// else{
+//     console.log('Temperature is greater than 50')
+
+// }
+// console.log('Executed!')
+
+
+const score = 200
+
+if (score>100) {
+    // const power = 'fly'
+    let power = 'fly'
+    // var power = 'fly'
+    console.log(`User power: ${power}`)
+}
+// console.log(`User power: ${power}`)
+
+// const balance = 800
+
+// if (balance>500) console.log('Test'), console.log('kk'); // Implicit scope!
+
+// if (balance<500) {
+//     console.log('Less than ')
+// }
+// else if(balance<750){
+//     console.log('Less than 750')
+
+// }
+// else if(balance<900){
+//     console.log('Less than 900')
+// }
+
+// else{
+//     console.log("It's 1000")
+// }
+
+//Logical operators
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+// if (userLoggedIn && debitCard && 2==2) {
+//     console.log('Allow to buy course')
+
+// }
+// if(loggedInFromEmail || loggedInFromGoogle) {
+//     console.log('User logged in')
+    
+// }
+
+
+
+
+
