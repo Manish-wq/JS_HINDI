9328484b22f03a5a02fa256e7c49281c4e97ee06
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-19 09:26:43 +0000

    loop4

---
+++ 05_iterations/objIteration.js
@@ -0,0 +1,30 @@
+const myObject = {
+    js: 'JavaScript',
+    cpp: 'C++',
+    rb: 'Ruby',
+    swift: 'Swift'
+
+}
+
+// for (const key in myObject) {
+//     console.log(`${key} shortcut for ${myObject[key]}`);
+// }
+
+// const programming = ['js', 'rb', 'py', 'java', 'c++']
+
+// for (const key in programming) {
+//     // console.log(key)
+//     console.log(programming[key]);
+// }
+
+
+const myMap = new Map()   // Map is not iterable
+myMap.set('In', 'India')
+myMap.set('USA', 'United States of America')
+myMap.set('Fr', 'France')
+myMap.set('In', 'India') // ordered and unique value.
+
+
+// for (const key in myMap) {
+//    console.log(key)
+// }
