0fb102f721ba2148c872ac615dac57027373707c
Manish-wq <61299240+Manish-wq@users.noreply.github.com>
2023-11-19 09:26:53 +0000

    loop5

---
+++ 05_iterations/while _doWhile_loop.js
@@ -0,0 +1,25 @@
+// let i = 5
+
+// while (i<=10) {
+//     console.log(`Value of i is ${i}`)
+//     i = i+2
+// }
+
+// let myArray = ['flash', 'batman', 'superman']
+// let i = 1
+// while(i<myArray.length){
+//     console.log(`Value is ${myArray[i]}`)
+//     i=i+1
+
+// }
+
+
+//___do_while_loop____________
+
+// let score = 4
+
+// do {
+//     console.log(`Score is ${score}`)
+//     score++
+// } while (score<=10);
+
